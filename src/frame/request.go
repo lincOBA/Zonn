8b41599e2bd8ad21aa9a23f4cd5dca1b5fe7b88d
lincOBA <linchaozhi@qq.com>
2021-03-06 17:49:40 +0800

    提交基于zinx的初版server模型

---
+++ src/frame/request.go
@@ -0,0 +1,18 @@
+package frame
+import "itf"
+type Request struct {
+	conn itf.IConnection //已经和客户端建立好的 链接
+	msg itf.IMessage     //客户端请求的数据
+}
+//获取请求连接信息
+func(r *Request) GetConnection() itf.IConnection {
+	return r.conn
+}
+//获取请求消息的数据
+func(r *Request) GetData() []byte {
+	return r.msg.GetData()
+}
+//获取请求的消息的ID
+func (r *Request) GetMsgID() uint32 {
+	return r.msg.GetMsgId()
+}
